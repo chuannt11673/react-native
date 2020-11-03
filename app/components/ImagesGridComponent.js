dd43f159e876a22f91980bed2ae605481c4210a8
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2020-11-03 11:56:25 +0700

    update ImageGrid

--- app/components/ImagesGridComponent.js
+++ app/components/ImagesGridComponent.js
@@ -1,53 +1,101 @@
-import React from 'react'
-import { Dimensions, StyleSheet, View, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Image } from 'react-native'
 
 import colors from '../config/color'
+import commonConsts from '../shared/consts/CommonConsts'
 
-export default function ImagesGridComponent() {
-    const images = [
-        'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        'https://images.unsplash.com/photo-1474073705359-5da2a8270c64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        'https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    ];
-    const windowWidth = Dimensions.get('window').width;
+export default function ImagesGridComponent(props) {
+    const [imageStyles, setImageStyles] = useState(props.images.map(_ => {
+        return {};
+    }));
     const render = (item, index) => {
         return (
-            <Image key={index} style={[getStyle(item, index), styles.item]} source={{ uri: item }} resizeMode='cover' />
+            <Image key={index} style={[styles.item, imageStyles[index]]} source={{ uri: item }}/>
         )
     }
-    const getStyle = (item, index) => {
-        if (index === 0) {
-            return {
-                width: '60%',
-                height: windowWidth
-            }
+    const getStyle = (index, selectMode) => {
+        if (selectMode === mode.horizontal) {
+            return getHorizontalStyle(props.images.length - 1, index);
         }
-        return {
-            width: '40%',
-            height: windowWidth / (images.length - 1),
-            position: 'absolute',
-            right: 0,
-            top: index === 1 ? 0 : windowWidth / (images.length - 1) * (index - 1)
-        }
+
+        return getVerticalStyle(props.images.length - 1, index);
     }
+
+    const onInit = () => {
+        Image.getSize(props.images[0], (width, height) => {
+            const selectMode = width < height ? mode.horizontal : mode.vertical;
+            const customStyles = props.images.map((item, index) => {
+                return getStyle(index, selectMode);
+            });
+            setImageStyles(customStyles);
+        });
+    }
+    onInit();
     
     return (
         <View style={styles.gridHorizontal}>
             {
-                images.map((item, index) => render(item, index))
+                props.images.map((item, index) => render(item, index))
             }
         </View>
     )
 }
 
+const mode = { horizontal: 1, vertical: 2 }
+const gridHeight = commonConsts.windowHeight * 0.6;
 const styles = StyleSheet.create({
     gridHorizontal: {
         width: '100%',
-        backgroundColor: colors.secondary,
-    },
-    item: {
-        borderColor: colors.grey,
-        borderWidth: 2
+        height: gridHeight,        
     }
 })
+
+const getHorizontalStyle = (itemLength, index) => {
+    itemLength =  itemLength <= 3 ? itemLength : 3;
+
+    if (index === 0) {
+        return {
+            width: '60%',
+            height: gridHeight,
+            borderColor: colors.white,
+            borderWidth: 1
+        }
+    } else if (index <= 3) {
+        return {
+            width: '40%',
+            height: gridHeight / itemLength,
+            position: 'absolute',
+            right: 0,
+            top: index === 1 ? 0 : gridHeight / itemLength * (index - 1),
+            borderColor: colors.white,
+            borderWidth: 1
+        }
+    }
+
+    return {};
+}
+
+const getVerticalStyle = (itemLength, index) => {
+    itemLength =  itemLength <= 3 ? itemLength : 3;    
+
+    if (index === 0) {
+        return {
+            width: commonConsts.windowWidth,
+            height: gridHeight * 0.6,
+            borderColor: colors.white,
+            borderWidth: 1
+        }
+    } else if (index <= 3) {
+        return {
+            width: commonConsts.windowWidth / itemLength,
+            height: gridHeight * 0.4 ,
+            position: 'absolute',
+            bottom: 0,
+            left: index === 1 ? 0 : commonConsts.windowWidth / itemLength * (index - 1),
+            borderColor: colors.white,
+            borderWidth: 1
+        }
+    }
+
+    return {};
+}
--- app/screens/DiaryScreen.js
+++ app/screens/DiaryScreen.js
@@ -40,7 +40,15 @@ export default function DiaryScreen() {
                     name={item.name}
                     message={item.message}
                 />
-                <ImagesGridComponent />
+                <ImagesGridComponent
+                    images={[
+                        'https://images.unsplash.com/photo-1510832198440-a52376950479?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                    ]}
+                />
             </View>
         );
     };
