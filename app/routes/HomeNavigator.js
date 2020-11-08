8a75de2824ea17a566f3dbcbcf44186b5d61156a
chuan nguyen <chuannt11673@gmail.com>
2020-11-08 23:41:32 +0700

    dating page

--- app/routes/HomeNavigator.js
+++ app/routes/HomeNavigator.js
@@ -1,15 +1,16 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SimpleLineIcons } from '@expo/vector-icons';
+import * as React from 'react'
+import { StyleSheet } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { SimpleLineIcons } from '@expo/vector-icons'
 
-import MessageScreen from '../screens/MessageScreen';
-import DiaryScreen from '../screens/DiaryScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import MessageScreen from '../screens/MessageScreen'
+import DiaryScreen from '../screens/DiaryScreen'
+import ProfileScreen from '../screens/ProfileScreen'
 
-import colors from '../config/color';
+import colors from '../config/color'
+import DatingScreen from '../screens/DatingScreen'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 export default function HomeNavigator() {
 
@@ -18,7 +19,7 @@ export default function HomeNavigator() {
       screenOptions={
         ({ route }) => ({
           tabBarIcon: ({ focused }) => {
-            const color = focused ? colors.secondary : colors.black10;
+            const color = focused ? colors.secondary : colors.black10
 
             if (route.name === 'Message') {
               return <SimpleLineIcons name="bubbles" size={21} color={color} />
@@ -28,6 +29,10 @@ export default function HomeNavigator() {
               return <SimpleLineIcons name="globe" size={21} color={color} />
             }
 
+            if (route.name === 'Dating') {
+              return <SimpleLineIcons name="heart" size={21} color={color} />
+            }
+
             return <SimpleLineIcons name="user" size={21} color={color} />
           }
         })
@@ -35,9 +40,10 @@ export default function HomeNavigator() {
     >
       <Tab.Screen name="Message" component={MessageScreen} />
       <Tab.Screen name="Diary" component={DiaryScreen} />
+      <Tab.Screen name="Dating" component={DatingScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
